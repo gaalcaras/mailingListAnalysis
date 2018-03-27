31c8253ec4bfab17549eb394d40048cc2d872f50
batlab <gaby.alcaras@gmail.com>
2018-03-27 22:31:01 +0200

    Add draw method

--- test/test_thread.py
+++ test/test_thread.py
@@ -6,6 +6,7 @@ thread1 = Thread(thread1_data)
 
 def test_graph_data():
     assert thread1.data == {
+        'authors': 2,
         'depth': 3,
         'in_degree': 0.75,
         'star_nodes': 0
--- thread.py
+++ thread.py
@@ -2,7 +2,10 @@
 Email Thread class
 """
 
+from networkx.drawing.nx_agraph import graphviz_layout
 import networkx as nx
+
+import matplotlib.pyplot as plt
 import numpy as nmp
 import pandas as pd
 
@@ -23,7 +26,9 @@ class Thread(object):
 
         :data: panda dataframe
         """
-        self.graph.add_nodes_from(data.msg.unique())
+        for index, row in data[['msg', 'sender_name']].iterrows():
+            self.graph.add_node(row.msg,
+                                author=row.sender_name)
 
         edges = data[['msg', 'reply_to']].dropna().values.tolist()
         self.graph.add_edges_from(edges)
@@ -40,11 +45,30 @@ class Thread(object):
         """
         degrees = [d for n, d in self.graph.in_degree]
         result = {
+            'authors': len(self.emails.sender_name.unique()),
             'depth': nx.dag_longest_path_length(self.graph),
             'in_degree': nmp.mean(degrees),
             'star_nodes': sum(d > 1 for d in degrees)
         }
         return result
+
+    def draw(self):
+        """Display simple drawing of thread graph"""
+
+        plt.subplot(121)
+
+        pos = graphviz_layout(self.graph, prog='dot')
+
+        # Colors
+        cmap = plt.get_cmap('viridis')
+        color = cmap(nmp.linspace(0, 1, self.data['authors']))
+        color_d = dict(zip(self.emails.sender_name.unique(), color))
+        author_seq = nx.get_node_attributes(self.graph, 'author').values()
+        color_seq = [color_d[n] for n in author_seq]
+
+        nx.draw(self.graph, pos, node_color=color_seq)
+
+        plt.show()
 
     def graph_info(self):
         """Print graph info"""
