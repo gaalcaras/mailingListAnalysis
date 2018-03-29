550e9c46f475ca08d9c6e1dfba848c20290a78c7
batlab <gaby.alcaras@gmail.com>
2018-03-30 01:08:36 +0200

    Update thread to match new csv columns

--- test/test_thread.py
+++ test/test_thread.py
@@ -5,9 +5,11 @@ thread1_data = pandas.read_csv('data/test_thread1.csv')
 thread1 = Thread(thread1_data)
 
 def test_graph_data():
-    assert thread1.data == {
+    assert dict(thread1.data) == {
         'authors': 2,
-        'depth': 3,
-        'in_degree': 0.75,
-        'star_nodes': 0
+        'depth': 4,
+        'in_degree': 0.8,
+        'star_nodes': 0,
+        'emails': 4,
+        'thread': 149519831100002,
     }
--- thread.py
+++ thread.py
@@ -2,6 +2,7 @@
 Email Thread class
 """
 
+from collections import OrderedDict
 from networkx.drawing.nx_agraph import graphviz_layout
 import networkx as nx
 
@@ -12,6 +13,8 @@ import pandas as pd
 class Thread(object):
 
     """Email Thread"""
+
+    cols = ['thread', 'emails', 'authors', 'depth', 'in_degree', 'star_nodes']
 
     def __init__(self, data):
         self.emails = data
@@ -26,11 +29,11 @@ class Thread(object):
 
         :data: panda dataframe
         """
-        for index, row in data[['msg', 'sender_name']].iterrows():
-            self.graph.add_node(row.msg,
-                                author=row.sender_name)
+        for row in data[['msg_id', 'from_name']].itertuples():
+            self.graph.add_node(row.msg_id,
+                                author=row.from_name)
 
-        edges = data[['msg', 'reply_to']].dropna().values.tolist()
+        edges = data[['msg_id', 'in_reply_to']].dropna().values.tolist()
         self.graph.add_edges_from(edges)
 
     def data_frame(self):
@@ -44,12 +47,15 @@ class Thread(object):
         Return thread data as dictionary
         """
         degrees = [d for n, d in self.graph.in_degree]
-        result = {
-            'authors': len(self.emails.sender_name.unique()),
-            'depth': nx.dag_longest_path_length(self.graph),
-            'in_degree': nmp.mean(degrees),
-            'star_nodes': sum(d > 1 for d in degrees)
-        }
+        row = [
+            self.emails['thread'].tolist()[0],
+            self.emails.shape[0],
+            len(self.emails.from_name.unique()),
+            nx.dag_longest_path_length(self.graph),
+            nmp.mean(degrees),
+            sum(d > 1 for d in degrees)
+        ]
+        result = OrderedDict(zip(self.cols, row))
         return result
 
     def draw(self):
@@ -62,7 +68,7 @@ class Thread(object):
         # Colors
         cmap = plt.get_cmap('viridis')
         color = cmap(nmp.linspace(0, 1, self.data['authors']))
-        color_d = dict(zip(self.emails.sender_name.unique(), color))
+        color_d = dict(zip(self.emails.from_name.unique(), color))
         author_seq = nx.get_node_attributes(self.graph, 'author').values()
         color_seq = [color_d[n] for n in author_seq]
 
