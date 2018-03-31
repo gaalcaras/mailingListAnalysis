e2b8aea7e4df89902cd025659fd36695f6458a40
batlab <gaby.alcaras@gmail.com>
2018-03-31 16:55:43 +0200

    Update thread tests and rename graph into tree

--- test/test_thread.py
+++ test/test_thread.py
@@ -15,26 +15,30 @@ THREAD2_DATA = pandas.read_csv('data/test_thread2.csv',
                                infer_datetime_format=True)
 THREAD2 = Thread(THREAD2_DATA)
 
+def test_thread_data():
+    assert dict(THREAD1.data) == {
+        'authors': 2,
+        'depth': 4,
+        'in_degree': 0.8,
+        'star_nodes': 0,
+        'duration': pandas.Timedelta(days=4, hours=10, minutes=58, seconds=26),
+        'emails': 4,
+        'thread': 149519831100002,
+    }
 
-class TestGraphData(object):
-    def test_thread1(self):
-        assert dict(THREAD1.data) == {
-            'authors': 2,
-            'depth': 4,
-            'in_degree': 0.8,
-            'star_nodes': 0,
-            'duration': pandas.Timedelta(days=4, hours=10, minutes=58, seconds=26),
-            'emails': 4,
-            'thread': 149519831100002,
-        }
+    assert dict(THREAD2.data) == {
+        'authors': 4,
+        'depth': 7,
+        'in_degree': 0.93,
+        'duration': pandas.Timedelta(days=12, hours=22, minutes=43, seconds=57),
+        'star_nodes': 3,
+        'emails': 15,
+        'thread': 111896264700001,
+    }
 
-    def test_thread2(self):
-        assert dict(THREAD2.data) == {
-            'authors': 4,
-            'depth': 7,
-            'in_degree': 0.93,
-            'duration': pandas.Timedelta(days=12, hours=22, minutes=43, seconds=57),
-            'star_nodes': 3,
-            'emails': 15,
-            'thread': 111896264700001,
-        }
+def test_tree_data():
+    assert len(THREAD1.tree.nodes) == 5
+    assert len(THREAD1.tree.edges) == 4
+
+    assert len(THREAD2.tree.nodes) == 15
+    assert len(THREAD2.tree.edges) == 14
--- thread.py
+++ thread.py
@@ -20,22 +20,23 @@ class Thread(object):
     def __init__(self, data):
         self.emails = data
 
-        self.graph = nx.DiGraph()
-        self._build_graph(data)
+        self.tree = nx.DiGraph()
+
+        self._build_tree(data)
 
         self.data = self._build_data()
 
-    def _build_graph(self, data):
-        """Build Graph object from email data
+    def _build_tree(self, data):
+        """Build tree Graph object from email data
 
         :data: panda dataframe
         """
         for row in data[['msg_id', 'from_email']].itertuples():
-            self.graph.add_node(row.msg_id,
+            self.tree.add_node(row.msg_id,
                                 author=row.from_email)
 
         edges = data[['msg_id', 'in_reply_to']].dropna().values.tolist()
-        self.graph.add_edges_from(edges)
+        self.tree.add_edges_from(edges)
 
     def data_frame(self):
         """
@@ -47,13 +48,13 @@ class Thread(object):
         """
         Return thread data as dictionary
         """
-        degrees = [d for n, d in self.graph.in_degree]
+        degrees = [d for n, d in self.tree.in_degree]
         row = [
             self.emails['thread'].tolist()[0],
             self.emails.shape[0],
             len(self.emails.from_email.unique()),
             max(self.emails.date) - min(self.emails.date),
-            nx.dag_longest_path_length(self.graph),
+            nx.dag_longest_path_length(self.tree),
             round(nmp.mean(degrees), 2),
             sum(d > 1 for d in degrees)
         ]
@@ -61,26 +62,26 @@ class Thread(object):
         return result
 
     def draw(self):
-        """Display simple drawing of thread graph"""
+        """Display simple drawing of thread tree graph"""
 
         plt.subplot(121)
 
-        pos = graphviz_layout(self.graph, prog='dot')
+        pos = graphviz_layout(self.tree, prog='dot')
 
         # Colors
-        cmap = plt.get_cmap('viridis')
+        cmap = plt.get_cmap('jet')
         color = cmap(nmp.linspace(0, 1, self.data['authors']))
         color_d = dict(zip(self.emails.from_email.unique(), color))
-        author_seq = nx.get_node_attributes(self.graph, 'author').values()
+        author_seq = nx.get_node_attributes(self.tree, 'author').values()
         color_seq = [color_d[n] for n in author_seq]
 
-        nx.draw(self.graph, pos, node_color=color_seq)
+        nx.draw(self.tree, pos, node_color=color_seq)
 
         plt.show()
 
     def graph_info(self):
-        """Print graph info"""
-        print(nx.info(self.graph))
+        """Print tree info"""
+        print(nx.info(self.tree))
 
     def open(self):
         """Open thread in web browser"""
