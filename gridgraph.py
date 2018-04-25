2f4bc8df435b1890176a0e58e9c4d0218be1e3eb
batlab <gaby.alcaras@gmail.com>
2018-04-25 19:00:47 +0200

    gridgraph: switch to template based generation
    Data and page generation were previously combined, which led to slow
    page edition. They are now separated, using HTML templates to avoid
    writing too much HTML markup in python file.

--- gridgraph.py
+++ gridgraph.py
@@ -1,94 +1,58 @@
 import os
 from tqdm import tqdm
 
-def gridgraph(ml, thread_ids, title):
-    draw_trees(ml, thread_ids)
-    body = ''
+def gridgraph(threads, filepath):
+    """Generate HTML grid page.
 
-    for thread in thread_ids:
-        body += tree_img(ml.thread(thread))
-
-    make_page('assets/html/{}.html'.format(title), title, body)
-
-def tree_img(thread):
-    img = '<span class="tree"'
-    for k, v in thread.data.items():
-        img += ' data-{}="{}"'.format(k, v)
-
-    img += '>\n'
-
-    img += '<a href="http://public-inbox.org/git/{}"'.format(thread.data['thread'])
-    img += ' target="_blank"'
-    img += ' title="{}"'.format(thread.data['subject'])
-    img += '>\n'
-    img += '<img'
-
-    img += ' src="../img/tree/{}.svg"'.format(thread.data['thread'])
-    img += ' alt="{}"'.format(thread.data['thread'])
-    img += '/>\n</a>\n'
-    img += '</span>\n'
-
-    return img
-
-def draw_trees(ml, thread_ids):
-    """Draw trees for each thread.
-
-    :ml: MailingList object storing the threads
-    :thread_ids: list of thread IDs
+    :threads: dataframe with threads data
+    :filepath: path of output HTML file
     """
 
-    for thread in tqdm(thread_ids, desc='Generating tree images'):
-        ml.thread(thread).draw_tree(save=True)
+    # Load HTML template
+    with open('assets/html/gridgraph.html', 'r') as temp:
+        template = temp.read()
 
-def make_page(filepath, title, body):
-    """Create HTML page.
+    # Make grid
+    grid = ''
+    for thread in threads.to_dict(orient='records'):
+        grid += span_tree(thread)
 
-    :filepath: target html file
-    :title: page title
-    :body: content of the page
-    """
+    template = template.format(grid=grid)
 
-    template = """<!doctype html>
-
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <script src="../js/gridgraph.js"></script>
-  <link rel="stylesheet" type="text/css" href="../css/gridgraph.css"><Paste>
-  <title>{}</title>
-</head>
-
-<body>
-<div id="controls">
-<form>
-<fieldset>
-<p>
-<label>Image size</label>
-<input type="range" min="100" max="500" step="10" value="300" id="img-size" oninput="changeImageSize(this.value)">
-</p>
-</fieldset>
-<fieldset id="sort">
-<p>
-<label>Crit1</label>
-<select id="crit1" class="criteria" onchange="reorderGrid()"></select>
-</p>
-<p>
-<label>Crit2</label>
-<select id="crit2" class="criteria" onchange="reorderGrid()"></select>
-</p>
-</fieldset>
-</form>
-</div>
-<div id="grid">
-{}
-</div>
-</body>
-</html>
-    """.format(title, body)
-
+    # Write to file
     dirname = os.path.dirname(filepath)
     if not os.path.isdir(dirname):
         os.makedirs(dirname)
 
     with open(filepath, 'w') as page:
         page.write(template)
+
+def span_tree(thread):
+    """Generate tree span for grid list.
+
+    :thread: dict of thread data
+    """
+    # Generate span attributes
+    span_att = ''
+    for key, value in thread.items():
+        span_att += ' data-{}="{}"'.format(key, value)
+
+    thread['span_att'] = span_att
+
+    # Load template
+    with open('assets/html/span.html', 'r') as temp:
+        span = temp.read()
+
+    span = span.format(**thread)
+
+    return span
+
+def draw_trees(mailing_list, thread_ids):
+    """Draw trees for each thread.
+
+    :mailing_list: MailingList object storing the threads
+    :thread_ids: list of thread IDs
+    """
+
+    for thread in tqdm(thread_ids, desc='Generating tree images'):
+        mailing_list.thread(thread).draw_tree(save=True)
