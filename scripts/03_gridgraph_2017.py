fbffc6613bc079a06b5c93d5dc9f55870b050b3c
batlab <gaby.alcaras@gmail.com>
2018-05-22 17:26:59 +0200

    scripts: experiment with metrics for gridgraph

--- scripts/03_gridgraph_2017.py
+++ scripts/03_gridgraph_2017.py
@@ -3,12 +3,19 @@ Generate an HTML grid of tree graphs from the 2017 threads.
 """
 
 import pandas as pd
+import numpy as np
 from gridgraph import gridgraph
 
 threads = pd.read_csv('data/working/threads2017.csv')
 
+# Add new metrics to refine typology
+threads['deg_delta'] = round((np.log1p(threads['deg_max']) / np.log1p(threads['emails'])), 2)
+threads['star_nodes_i'] = round((np.log1p(threads['star_nodes']) / np.log1p(threads['emails'])), 2)
+
 # Keep only threads containing 10 emails
-threads10 = threads[threads.emails == 10]
+threads_e10 = threads[threads.emails == 10]
+threads_h3 = threads[threads.h_index == 3]
 
 # Build the grid of graphs
-gridgraph(threads10, 'output/gridgraph_17_10.html')
+gridgraph(threads_e10, 'output/gridgraph_17_e10.html')
+gridgraph(threads_h3, 'output/gridgraph_17_h3.html')
