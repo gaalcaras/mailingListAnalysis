4a09e282a3ee8d03b68990fba459808f4a7a0b53
batlab <gaby.alcaras@gmail.com>
2018-06-01 18:25:44 +0200

    data: update data download

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-wget -O data/working/year2017_threaded.csv https://archive.org/download/year2017_threaded/year2017_threaded.csv
+wget -O data/raw/2017_03_30_git_emails.csv https://dl.gaalcaras.com/mla/data/raw/2017_03_30_git_emails.csv
+wget -O data/working/emails_threaded.csv https://dl.gaalcaras.com/mla/data/working/emails_threaded.csv
+wget -O data/working/threads.csv https://dl.gaalcaras.com/mla/data/working/threads.csv
+wget -O data/working/threads2017.csv https://dl.gaalcaras.com/mla/data/working/threads2017.csv
+wget -O data/working/threads2017_labeled.csv https://dl.gaalcaras.com/mla/data/working/threads2017_labeled.csv
+wget -O data/working/year2017_threaded.csv https://dl.gaalcaras.com/mla/data/working/year2017_threaded.csv
